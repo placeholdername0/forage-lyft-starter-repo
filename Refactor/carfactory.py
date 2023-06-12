e3c4c7f55a325c9e703b431507caf92833348d08
placeholdername0 <132001266+placeholdername0@users.noreply.github.com>
2023-06-11 20:45:09 -0400

    added which car has which tire type. I didn't see it listed before the example

--- Refactor/carfactory.py
+++ Refactor/carfactory.py
@@ -5,6 +5,7 @@ from abc import ABC, abstractmethod
 from serv_interface import Serviceable
 from engines import Willoughby_E, Sternman_E, Capulet_E
 from batteries import Spindler_B, Nubbin_B
+from tires import Carrigan, Octoprime
 """
 from engine.model.calliope import Calliope
 
@@ -36,31 +37,36 @@ class CarFactory():
     def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage,tire_array):
         car_engine = Capulet_E(last_service_mileage, current_mileage)
         car_battery = Spindler_B(last_service_date, current_date)
-        newCar = Car(car_battery, car_engine)
+        tires = Carrigan(tire_array)
+        newCar = Car(car_battery, car_engine, tires)
         return newCar
 
     def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage, tire_array):
         car_engine = Willoughby_E(last_service_mileage, current_mileage)
         car_battery = Spindler_B(last_service_date, current_date)
-        newCar = Car(car_battery, car_engine)
+        tires = Octoprime(tire_array)
+        newCar = Car(car_battery, car_engine, tires)
         return newCar
 
     def create_palindrome(current_date, last_service_date, warning_light_on, tire_array):
         newCar = Car(current_date, last_service_date, warning_light_on)
         car_engine = Sternman_E(warning_light_on)
         car_battery = Spindler_B(last_service_date, current_date)
-        newCar = Car(car_battery, car_engine)
+        tires = Carrigan(tire_array)
+        newCar = Car(car_battery, car_engine, tires)
         return newCar
 
     def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage, tire_array):
         car_engine = Willoughby_E(last_service_mileage, current_mileage)
         car_battery = Nubbin_B(last_service_date, current_date)
-        newCar = Car(car_battery, car_engine)
+        tires = Octoprime(tire_array)
+        newCar = Car(car_battery, car_engine, tires)
         return newCar
       
     def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage, tire_array):
         car_engine = Capulet_E(last_service_mileage, current_mileage)
         car_battery = Nubbin_B(last_service_date, current_date)
+        tires = Carrigan(tire_array)
         newCar = Car(car_battery, car_engine)
         return newCar
 
@@ -70,8 +76,9 @@ class Car(Serviceable):
         Car_Engine = engine 
         Car_Batt = batter
         last_service_date = last_serv_date
+        Car_tires = tires
     def needs_service(self):
-        if Car.Car_Batt.needs_service() == True or Car.Car_Engine.needs_service() == True:
+        if Car.Car_Batt.needs_service() == True or Car.Car_Engine.needs_service() == True or Car.Car_tires.needs_service():
             return True
         else:
             return False
